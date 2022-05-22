e2ebe8c9b38b12b87386210df75dc36ab2196592
mjsf1234 <mrityunjaysaraf5678@gmail.com>
2022-05-22 23:57:04 +0530

    update

--- Tree/TreeUse.cpp
+++ Tree/TreeUse.cpp
@@ -3,6 +3,27 @@ using namespace std;
 #include <queue>
 #include "TreeNodeClass.h"
 
+/* ----------------Content---------------
+! TODO: Print the Node as well as their children recursively
+! TODO: Print the tree nodes levelwise
+! TODO: Take input function levelwise
+! TODO: take input from the users and return the address of the root
+! TODO: count the number of Node recursively
+! TODO: Number of nodes in the given tree
+! TODO: Node with Max Data
+! TODO: Find the height of the tree
+! TODO: Print all the element at level k
+! TODO: Number of lead Node
+! TODO: Find whether X in present in tree or not
+! TODO:  find the node whose value is greater than x ;
+! TODO: Node with maximum child sum
+! TODO: Replace the node with its depth value
+! TODO: check if the tree is strucutal similar or not
+! TODO: Find the next largest value
+! TODO: find the second largest value in the generic tree
+! TODO: Second Largest Element In Tree
+*/
+
 // TODO: Print the Node as well as their children recursively
 void printTree(TreeNode<int> *root)
 {
@@ -226,14 +247,132 @@ int getLargeNodeCount(TreeNode<int> *root, int x)
     int count = 0;
     if (root->data > x)
     {
-        return 1;
+        count += 1;
     }
     for (int i = 0; i < root->children.size(); i++)
     {
         count += getLargeNodeCount(root->children[i], x);
     }
+    return count;
+};
+
+// TODO : Node with maximum child sum
+
+int getTheNodeSum(TreeNode<int> *root)
+{
+    int sum = root->data;
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        sum += root->children[i]->data;
+    }
+    return sum;
 }
 
+TreeNode<int> *maxSumNode(TreeNode<int> *root)
+{
+    int max;
+    TreeNode<int> *Node;
+
+    int temp = getTheNodeSum(root);
+    if (temp > max)
+    {
+        max = temp;
+        Node = root;
+    }
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        int temp1 = getTheNodeSum(root->children[i]);
+        if (temp1 > max)
+        {
+            max = temp1;
+            Node = root->children[i];
+        }
+    }
+    return Node;
+};
+
+// TODO : Replace the node with its depth value
+void replaceWithDepthValue(TreeNode<int> *root)
+{
+    root->data = 0;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+    }
+};
+
+// TODO : check if the tree is strucutal similar or not
+bool areIdentical(TreeNode<int> *root1, TreeNode<int> *root2)
+{
+    bool check = false;
+    if (root1->children.size() != root2->children.size())
+    {
+        return false;
+    }
+    if (root1->data == root2->data)
+    {
+        check = true;
+    }
+    for (int i = 0; i < root1->children.size(); i++)
+    {
+        bool temp = areIdentical(root1->children[i], root2->children[i]);
+        if (temp = false)
+        {
+            check = false;
+            break;
+        }
+    }
+    return check;
+};
+
+// TODO : Find the next largest value
+TreeNode<int> *getNextLargerElement(TreeNode<int> *root, int x)
+{
+    TreeNode<int> *next = NULL;
+    if (root->data > x)
+    {
+        next = root;
+    }
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        TreeNode<int> *tempNode = getNextLargerElement(root->children[i], x);
+        if (next != NULL)
+        {
+            if (tempNode != NULL && tempNode->data < next->data)
+            {
+                next = tempNode;
+            }
+        }
+        else
+        {
+            if (tempNode != NULL)
+            {
+                next = tempNode;
+            }
+        }
+    }
+    return next;
+};
+
+// TODO : find the second largest value in the generic tree
+
+TreeNode<int> *getSecondLargestNode(TreeNode<int> *root)
+{
+    TreeNode<int> *largest = NULL;
+    TreeNode<int> *secondLargest = NULL;
+    if (root != NULL)
+    {
+        largest = root;
+    }
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        TreeNode<int> *temp = getSecondLargestNode(root->children[i]);
+        if (temp->data > largest->data)
+        {
+            largest = temp;
+        };
+    }
+}
 int main()
 {
     /* TreeNode<int> *root = new TreeNode<int>(1);
@@ -254,7 +393,9 @@ int main()
     // cout << endl
     // cout << "total leaf is " << getLeafNodeCount(root) << endl;
 
-    cout << "present " << isPresent(root, 8);
+    // cout << "present " << isPresent(root, 8);
+    // cout << getLargeNodeCount(root, 6);
+    cout << "answer" << maxSumNode(root)->data;
     delete root;
 
     return 0;
