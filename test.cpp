9dada8b184323da6ca76b2027462aea64e587c68
mjsf1234 <mrityunjaysaraf5678@gmail.com>
2022-06-10 00:07:10 +0530

    update

--- test.cpp
+++ test.cpp
@@ -6,16 +6,20 @@ using namespace std;
 int maxProfit(int a[], int size)
 {
     int buy = INT32_MAX;
-    int sell = INT32_MIN;
+    int sell = 0;
     int ans = 0;
     for (int i = 0; i < size; i++)
     {
         buy = min(buy, a[i]);
-        sell = max()
+        int profit = a[i] - buy;
+        ans = max(ans, profit);
     }
+    return ans;
 }
 int main()
 {
+    int a[] = {7, 1, 5, 3, 6, 4};
+    cout << maxProfit(a, 6);
 
     return 0;
 }
