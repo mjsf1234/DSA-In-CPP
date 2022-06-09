63d2e1b1690cc6779fb6700c111f2a4aff039758
mjsf1234 <mrityunjaysaraf5678@gmail.com>
2022-06-09 23:58:00 +0530

    update

--- test.cpp
+++ test.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
+#include <vector>
 
 // todo: find the maximum
-
+int maxProfit(int a[], int size)
+{
+    int buy = INT32_MAX;
+    int sell = INT32_MIN;
+    int ans = 0;
+    for (int i = 0; i < size; i++)
+    {
+        buy = min(buy, a[i]);
+        sell = max()
+    }
+}
 int main()
 {
 
