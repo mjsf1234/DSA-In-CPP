05a4141b6896aceeb7b489879c47ed7b7ecaab6b
mjsf1234 <mrityunjaysaraf5678@gmail.com>
2022-06-15 21:26:00 +0530

    update

--- Hashmap/Hashmap.cpp
+++ Hashmap/Hashmap.cpp
@@ -4,47 +4,58 @@ using namespace std;
 
 int highestFrequency(int arr[], int n)
 {
-    unordered_map<int, int> map;
-    for (int i = 0; i < n; i++)
+    int ans = arr[1];
+    int max = 0;
+    for (int i = 0; i < n - 1; i++)
     {
-        if (map.count(arr[i]) > 0)
+        int count = 1;
+        for (int j = i + 1; j < n; j++)
         {
-            arr[i] += 1;
+            if (arr[i] == arr[j])
+            {
+                count += 1;
+            }
         }
-        map[arr[i]] = 1;
+        if (count > max)
+        {
+            ans = arr[i];
+            max = count;
+        }
     }
-    for (int i = map.begin(); i != map.end(); i++)
-    {
-    }
+    return ans;
 }
 
 int main()
 {
-    unordered_map<string, int> ourmap; // unordered_map(key datatype, values datatype)
-    // insertion operation
-    ourmap["first"] = 1;
-    ourmap["second"] = 2;
-    ourmap["third"] = 3;
-    // another methode of insertion
-    pair<string, int> p("forth", 4);
-    ourmap.insert(p);
+    /* unordered_map<string, int> ourmap; // unordered_map(key datatype, values datatype)
+     // insertion operation
+     ourmap["first"] = 1;
+     ourmap["second"] = 2;
+     ourmap["third"] = 3;
+     // another methode of insertion
+     pair<string, int> p("forth", 4);
+     ourmap.insert(p);
 
-    // access the element
-    cout << ourmap.at("first") << endl; // this will throws the error of "out rof range" in case key not found
-    cout << ourmap["first"] << endl;    // this will insert the zero(default) in case key not found
+     // access the element
+     cout << ourmap.at("first") << endl; // this will throws the error of "out rof range" in case key not found
+     cout << ourmap["first"] << endl;    // this will insert the zero(default) in case key not found
 
-    // check if the key is present or not
-    if (ourmap.count("first") > 0)
-    {
-        cout << "the value of first " << ourmap["first"] << endl;
-    }
+     // check if the key is present or not
+     if (ourmap.count("first") > 0)
+     {
+         cout << "the value of first " << ourmap["first"] << endl;
+     }
 
-    // size of map
-    cout << "the size of the map is " << ourmap.size() << endl;
+     // size of map
+     cout << "the size of the map is " << ourmap.size() << endl;
 
-    // erase
-    ourmap.erase("second");
-    cout << "the size of the map is " << ourmap.size() << endl;
+     // erase
+     ourmap.erase("second");
+     cout << "the size of the map is " << ourmap.size() << endl;*/
+
+    // higher frequency array;
+    int arr[] = {1, 2, 2, 1};
+    cout << highestFrequency(arr, 4);
 
     return 0;
 }
